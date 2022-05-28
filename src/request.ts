3d7e151ecb1973cc7e95b607e05c83c6e6d8de81
Danial Manavi <dmanavi@live.com>
2022-05-28 15:59:50 -0700

    JSON RPC request types.

---
+++ src/request.ts
@@ -0,0 +1,25 @@
+import { JsonRpcBase } from 'json_rpc_base';
+
+export type RequestBase<TParams = Array<any>> = JsonRpcBase & {
+  /**
+   * Method name
+   */
+  method: string;
+
+  /**
+   * Method arguments/parameters
+   */
+  params?: TParams;
+};
+
+export type InvokeRequest<TParams = Array<any>> = RequestBase<TParams> & {
+  /**
+   * Request identifier
+   */
+  id: string | number;
+};
+
+export type NotificationRequest<TParams = Array<any>> =
+  RequestBase<TParams> & {};
+
+export type Request = InvokeRequest | NotificationRequest;
