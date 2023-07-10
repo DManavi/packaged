0bdc26d7bf3d1da7add7f2e569e3957607ea109d
Danial Manavi <dmanavi@live.com>
2023-07-10 23:16:43 +0000

    updated package reading.

--- bin/remove_dev_deps.js
+++ bin/remove_dev_deps.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 const { resolve } = require('node:path');
+const { readFileSync } = require('node:fs');
 
 // load package.json
-const pkg = require(resolve(__dirname, '..', 'package.json'));
+const pkgStr = readFileSync(resolve(__dirname, '..', 'package.json'), 'utf-8');
+// parse JSON content
+const pkg = JSON.parse(pkgStr);
 
 // delete devDependencies
 delete pkg.devDependencies;
