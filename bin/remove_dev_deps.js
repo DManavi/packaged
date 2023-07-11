70ae4d2a9b7e2a5cef5188aa5e0118364dffda60
Danial Manavi <dmanavi@live.com>
2023-07-11 04:13:56 +0000

    fixed scripts.

--- bin/remove_dev_deps.js
+++ bin/remove_dev_deps.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 
-const { resolve } = require('node:path');
 const { readFileSync } = require('node:fs');
 
 // load package.json
-
-const pkgFilePath = resolve(__dirname, '..', 'package.json');
-
-const pkgStr = readFileSync(pkgFilePath, {
-  encoding: 'utf-8',
-});
+const pkgStr = readFileSync(0, 'utf-8');
 
 // parse JSON content
 const pkg = JSON.parse(pkgStr);
